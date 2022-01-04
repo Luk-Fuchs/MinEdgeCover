7fdb700e5017d348aa04f06070f32e27f44e08c0
Luk-Fuchs <83712035+Luk-Fuchs@users.noreply.github.com>
2022-01-04 18:09:09 +0100

    Program update

--- 3D Matching/Tests/Program.cs
+++ 3D Matching/Tests/Program.cs
@@ -11,43 +11,69 @@ namespace _3D_Matching
     {
         static void Main(string[] args)
         {
+
             var parameters = new Dictionary<String, double>();
-            parameters.Add("maxTime",200);
+            parameters.Add("maxTime", 2000);
             parameters.Add("maxIter", 10000);
             int n = 300;        //320
             double p3 = 0.01;
             double p2 = 0.1;
             double p1 = 0.5;
-            int iterations = 10;
+            int iterations = 1;
+
+
+            //var graph = Graph.BuildGraphFromCSV(@"C:\Users\LFU\Desktop\Masterarbeit\UnitTestDaten");
+            //var solver = new Solvers.Constructive();
+            //solver.initialize(graph);
+            //var res = solver.Run(parameters);
+
+
+
+
 
             var solvers = new List<IMinimumEdgecoveringSolver> {
-                //new HC(climbMode: "allNotOptimal",maxEdgeSwapSize:10),
+                //new BAB(),
+                //new Genetic("fast"),
+                //new Genetic("normal"),
+                //new Genetic("test"),
+                //new Genetic("linkedList"),
+                //new Constructive(),
+                //new HC(climbMode: "allNotOptimal",maxEdgeSwapSize:7),
                 //new HC(maxEdgeSwapSize:10),
-                new HC(climbMode: "alternatingSmallImprovements",maxEdgeSwapSize:5),
+                //new HC(climbMode: "alternatingSmallImprovements",maxEdgeSwapSize:5),
                 //new HC(maxEdgeSwapSize:15),
                 //new HC(maxEdgeSwapSize:20),
+                //new HC(climbMode: "alternatingPaths"),
                 //new HC(maxEdgeSwapSize:25),
                 //new HC(maxEdgeSwapSize:30),
-                //new Greedy("byDegree"),
-                //new Greedy("byNeighbourhoodSize"),
-                //new Greedy(mode: "byDegree"),
+                new Greedy("byDegree"),
+                new Greedy(mode: "byDegreeUpdatingAndMinSumSorting", sizeWeight: 4, variation: 1),
+                new Greedy(mode: "byDegreeUpdatingAndMinSumSorting", sizeWeight: 4, variation: 2),
+                new Greedy(mode: "byDegreeUpdatingAndMinSumSorting", sizeWeight: 5, variation: 1),
+                new Greedy(mode: "byDegreeUpdatingAndMinSumSorting", sizeWeight: 5, variation: 2),
+                //new Greedy(mode: "byNeighbourhoodSize"),
+                //new Greedy(mode: "bydegree"),
                 //new Greedy(mode: "byDegreeUpdating"),
                 //new Greedy(mode: "byDegreeUpdatingAndRegret"),  //scheint bis jetzt nicht sinnvoll
                 //new Greedy(mode: "byDegreeUpdating_V3"),
                 //new SAS(500),
                 //new RORTS(0,preCalculationTime:20),
-                //new RORTS(20,preCalculationTime:20),
-                //new RORTS(40,preCalculationTime:20,mode:"partialOptimal"),
-                //new RORTS(20,preCalculationTime:20, mode:"minDegree"),
+                //new RORTS(10,preCalculationTime:20),
+                //new RORTS(5,preCalculationTime:20,mode:"partialOptimal"),
+                //new RORTS(5,preCalculationTime:20, mode:"minDegree"),
                 //new RORTS(20,preCalculationTime:20,mode:"artificalThinning"),
-                //new ORTS(),
+                new ORTS(),
                 //new OnlineSolver(true),
                 //new OnlineSolver(false),
         };
 
+            //for (int i = 0; i < 10; i++)
+            //    for (int j = 0; j < 10; j++)
+            //        solvers.Add(new Greedy(mode: "byDegreeUpdatingAndMinSumSorting", sizeWeight: i, variation: j));
+
             var data = SolverTester.RunSolvers(solvers, 
                                                 parameters,
-                                                generationType: "random",            //"readIn", "random"
+                                                generationType: "readIn",            //"readIn", "random"
                                                 iterations:iterations,
                                                 n:n,p1:p1,p2:p2,p3:p3);
 
