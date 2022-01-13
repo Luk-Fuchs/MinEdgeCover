a56fb08354c3dbc6bfe88f7a05887257742c81ea
Luk-Fuchs <83712035+Luk-Fuchs@users.noreply.github.com>
2022-01-13 13:45:11 +0100

    save file for refactoring

--- 3D Matching/Solvers/Genetic.cs
+++ 3D Matching/Solvers/Genetic.cs
@@ -11,9 +11,13 @@ namespace _3D_Matching.Solvers
     {
         Random _random = new Random();
         String _type = "";
-        public Genetic(String type = "")
+        IMinimumEdgecoveringSolver _precalcSolver;
+        private bool _precalc;
+        public Genetic(String type = "", bool precalc = true, IMinimumEdgecoveringSolver precalcSolver= null)
         {
             _type = type;
+            _precalc = precalc;
+            _precalcSolver = precalcSolver;
         }
 
         public override String Name { get => this.GetType().Name + "|" + _type; }
@@ -27,38 +31,138 @@ namespace _3D_Matching.Solvers
 
             var uncoveredVertices = _graph.Vertices.ToList();
 
-            var bestResCount = _edges.Count;
+            var bestResCount = int.MaxValue;
             var vertexCount = _graph.Vertices.Count;
             //List<List<int>> bestPermutation = new List<List<int>>();
             var iteration = 0;
             var isCovored = new bool[vertexCount];
             List<List<int>> edges = new List<List<int>>();
 
-            if (_type == "normal")
-            {
-                edges = _edges.Select(_ => _.VerticesIds).OrderBy(_ => -_.Count).ToList();
-            }
-            else if (_type =="fast")
+            if (_type =="fast")
             {
                 edges = GenerateInitalPermutation();
             }
+            else if (_type =="test")
+            {
+                edges = GenerateInitalPermutationBySeveralLists();
+            }else if (_type == "precalc")
+            {
+                //var solver = new Greedy(mode: "byDegreeUpdatingAndMinSumSorting", sizeWeight: 4, variation: 0);
+                _precalcSolver.initialize(_graph);
+                var tmpParams = new Dictionary<String, double>();
+                tmpParams.Add("maxIter", 10);
+                tmpParams.Add("maxTime", 25);
+                var preRes = _precalcSolver.Run(tmpParams);
+                edges = preRes.cover.Concat(_edges.Where(_ => !preRes.cover.Contains(_))).Select(_=>_.VerticesIds).ToList();
+                bestResCount = preRes.cover.Count;
+            }
+            else 
+            {
+                edges = _edges.Select(_ => _.VerticesIds).OrderBy(_ => -_.Count).ToList();
+            }
 
+            if (_type == "normal" || _type == "fast" || _type == "test" || _type == "precalc")
+            {
+                int index = 0;
+                var tmp = new List<int>();
                 while (time.ElapsedMilliseconds < maxTime)
                 {
-                    iteration++;
                     isCovored = new bool[vertexCount];
-                    var index = _random.Next(edges.Count);
-                    var amountOfCoveredVertices = 0;
-                    var coverSize = 0;
+                    index = _random.Next(edges.Count);
 
-                    //var test = edges.ToList();
-
-                    var tmp = edges[index];
+                    tmp = edges[index];
                     edges.RemoveAt(index);
                     edges.Insert(0, tmp);
 
 
+                    var amountOfCoveredVertices = 0;
+                    var coverSize = 0;
+                    iteration++;
+
                     foreach (var edge in edges)
+                    {
+                        bool skip = false;
+                        foreach (var i in edge)
+                        {
+                            if (isCovored[i])
+                            {
+                                skip = true;
+                                break;
+
+                            }
+                        }
+                        if (skip)
+                            continue;
+
+                        foreach (var i in edge)
+                        {
+                            isCovored[i] = true;
+                            amountOfCoveredVertices++;
+                        }
+
+                        coverSize++;
+                        if (amountOfCoveredVertices == vertexCount || coverSize > bestResCount)
+                            break;
+                    }
+                    //Console.WriteLine(coverSize +"  " + (amountOfCoveredVertices == vertexCount));
+
+                    if (coverSize <= bestResCount && amountOfCoveredVertices == vertexCount)// && amountOfCoveredVertices==vertexCount)
+                    {
+                        bestResCount = coverSize;
+                        //bestPermutation = edges.ToList();
+                    }
+                    else if (bestResCount != int.MaxValue)
+                    {
+                        tmp = edges[0];
+                        edges.RemoveAt(0);
+                        edges.Insert(index, tmp);
+                    }
+
+                }
+
+            }
+            else if (_type == "linkedList")
+            {
+                var linkedEdges = new LinkedList<List<int>>(GenerateInitalPermutationBySeveralLists());
+
+                var swapItem = linkedEdges.First;
+                for (int i = 0; i < _edges.Count / 2; i++)
+                {
+                    swapItem = swapItem.Next;
+                }
+
+                var index = _edges.Count / 2;
+                while (time.ElapsedMilliseconds < maxTime)
+                {
+                    iteration++;
+                    isCovored = new bool[vertexCount];
+                    var offset = _random.Next(-5, 5);
+                    var amountOfCoveredVertices = 0;
+                    var coverSize = 0;
+
+                    //var test = edges.ToList();
+                    if (offset > 0)
+                    {
+                        offset = Math.Min(_edges.Count - index - 1, 5);
+                        for (int i = 0; i < offset; i++)
+                            swapItem = swapItem.Next;
+
+                    }
+                    if (offset < 0)
+                    {
+                        offset = Math.Max(-index + 1, -5);
+
+                        for (int i = 0; i > offset; i--)
+                            swapItem = swapItem.Previous;
+                    }
+                    index += offset;
+
+                    var toInsertFirst = swapItem.Previous;
+                    linkedEdges.Remove(toInsertFirst);
+                    linkedEdges.AddFirst(toInsertFirst);
+
+
+                    foreach (var edge in linkedEdges)
                     {
                         bool skip = false;
                         foreach (var i in edge)
@@ -83,14 +187,100 @@ namespace _3D_Matching.Solvers
                         if (amountOfCoveredVertices == vertexCount)
                             break;
                     }
-                    //Console.WriteLine(coverSize);
 
-                    if (coverSize <= bestResCount)// && amountOfCoveredVertices==vertexCount)
+                    if (coverSize <= bestResCount)
                     {
                         bestResCount = coverSize;
-                        //bestPermutation = edges.ToList();
                     }
                     else
+                    {
+                        linkedEdges.Remove(toInsertFirst);
+                        linkedEdges.AddBefore(swapItem, toInsertFirst);
+                    }
+                }
+                edges = linkedEdges.ToList();
+            }
+            else if (_type == "onlyPermuteLargeEdges")
+            {
+                int index = 0;
+                var tmp = new List<int>();
+                var edges1 = edges.Where(_ => _.Count == 1).ToList();
+                edges = edges.Where(_ => _.Count != 1).ToList();
+                var testCounter = 0;
+                while (time.ElapsedMilliseconds < maxTime)
+                {
+                    isCovored = new bool[vertexCount];
+                    index = _random.Next(edges.Count);
+
+                    tmp = edges[index];
+                    edges.RemoveAt(index);
+                    edges.Insert(0, tmp);
+
+
+                    var amountOfCoveredVertices = 0;
+                    var coverSize = 0;
+                    iteration++;
+
+                    foreach (var edge in edges)
+                    {
+                        bool skip = false;
+                        foreach (var i in edge)
+                        {
+                            if (isCovored[i])
+                            {
+                                skip = true;
+                                break;
+
+                            }
+                        }
+                        if (skip)
+                            continue;
+
+                        foreach (var i in edge)
+                        {
+                            isCovored[i] = true;
+                            amountOfCoveredVertices++;
+                        }
+
+                        coverSize++;
+                        if (amountOfCoveredVertices == vertexCount || coverSize > bestResCount)
+                            break;
+                    }
+                    foreach (var edge in edges1)
+                    {
+                        bool skip = false;
+                        foreach (var i in edge)
+                        {
+                            if (isCovored[i])
+                            {
+                                skip = true;
+                                break;
+
+                            }
+                        }
+                        if (skip)
+                            continue;
+
+                        foreach (var i in edge)
+                        {
+                            isCovored[i] = true;
+                            amountOfCoveredVertices++;
+                        }
+
+                        coverSize++;
+                        if (amountOfCoveredVertices == vertexCount || coverSize > bestResCount)
+                            break;
+                    }
+                    //Console.WriteLine(coverSize +"  " + (amountOfCoveredVertices == vertexCount));
+
+
+                    if (coverSize <= bestResCount && amountOfCoveredVertices == vertexCount)// && amountOfCoveredVertices==vertexCount)
+                    {
+                        bestResCount = coverSize;
+                        testCounter++;
+                        //bestPermutation = edges.ToList();
+                    }
+                    else/* if (bestResCount != int.MaxValue /*&& coverSize != bestResCount*/
                     {
                         tmp = edges[0];
                         edges.RemoveAt(0);
@@ -100,6 +290,122 @@ namespace _3D_Matching.Solvers
 
 
                 }
+                Console.WriteLine((double)iteration/testCounter);
+                edges = edges.Concat(edges1).ToList();
+            }
+            else if (_type == "prefereBadCovered")
+            {
+                int index = 0;
+                var tmp = new List<int>();
+                var edges1 = edges.Where(_ => _.Count == 1).ToList();
+                edges = edges.Where(_ => _.Count != 1).ToList();
+                var isCovoredByEdgeSize = new int[vertexCount];
+                while (time.ElapsedMilliseconds < maxTime)
+                {
+                    isCovored = new bool[vertexCount];
+
+
+                    index = _random.Next(1,edges.Count -1);
+
+                    foreach( var i in edges[index+1])
+                        if (isCovoredByEdgeSize[i] == 1)
+                        {
+                            index = index +1;
+                            break;
+                        }
+                    //foreach (var i in edges[index - 1])
+                    //    if (isCovoredByEdgeSize[i] == 1)
+                    //    {
+                    //        index--;
+                    //        break;
+                    //    }
+
+                    if (edges[index].Select(_=>isCovoredByEdgeSize[_]).Min() > edges[index+1].Select(_ => isCovoredByEdgeSize[_]).Min())
+                        index++;
+
+                    tmp = edges[index];
+                    edges.RemoveAt(index);
+                    edges.Insert(0, tmp);
+
+
+
+
+                    isCovoredByEdgeSize = new int[vertexCount];
+
+                    var amountOfCoveredVertices = 0;
+                    var coverSize = 0;
+                    iteration++;
+
+                    foreach (var edge in edges)
+                    {
+                        bool skip = false;
+                        foreach (var i in edge)
+                        {
+                            if (isCovoredByEdgeSize[i] != 0)
+                            {
+                                skip = true;
+                                break;
+
+                            }
+                        }
+                        if (skip)
+                            continue;
+
+                        foreach (var i in edge)
+                        {
+                            isCovoredByEdgeSize[i] = edge.Count;
+                            amountOfCoveredVertices++;
+                        }
+
+                        coverSize++;
+                        if (amountOfCoveredVertices == vertexCount || coverSize > bestResCount)
+                            break;
+                    }
+                    foreach (var edge in edges1)
+                    {
+                        bool skip = false;
+                        foreach (var i in edge)
+                        {
+                            if (isCovoredByEdgeSize[i] != 0)
+                            {
+                                skip = true;
+                                break;
+
+                            }
+                        }
+                        if (skip)
+                            continue;
+
+                        foreach (var i in edge)
+                        {
+                            isCovoredByEdgeSize[i] = edge.Count;
+                            amountOfCoveredVertices++;
+                        }
+
+                        coverSize++;
+                        if (amountOfCoveredVertices == vertexCount || coverSize > bestResCount)
+                            break;
+                    }
+                    //Console.WriteLine(coverSize + "  " + (amountOfCoveredVertices == vertexCount));
+
+                    if (coverSize <= bestResCount && amountOfCoveredVertices == vertexCount)// && amountOfCoveredVertices==vertexCount)
+                    {
+                        bestResCount = coverSize;
+                        //bestPermutation = edges.ToList();
+                    }
+                    else if (bestResCount != int.MaxValue)
+                    {
+                        tmp = edges[0];
+                        edges.RemoveAt(0);
+                        edges.Insert(index, tmp);
+                    }
+
+
+
+                }
+                edges = edges.Concat(edges1).ToList();
+            }
+
             //reconstruct:
             var result = new List<Edge>();
             isCovored = new bool[vertexCount];
@@ -159,6 +465,38 @@ namespace _3D_Matching.Solvers
             }
 
             return edges;
+        }
+        
+        private List<List<int>> GenerateInitalPermutationBySeveralLists() //slower that lambda
+        {
+
+            //generate structure:
+           
+            var edges = new List<List<int>>();
+            var edges1 = new List<List<int>>();
+            var edges2 = new List<List<int>>();
+            var edges3 = new List<List<int>>();
+            var pointerTwoEdge = 0;
+
+            foreach (var edge in _edges)
+            {
+                if (edge.Vertices.Count == 3)
+                {
+                    edges3.Add(edge.VerticesIds);
+                }
+                else if (edge.Vertices.Count == 1)
+                {
+                    edges1.Add(edge.VerticesIds);
+                }
+                else if (edge.Vertices.Count == 2)
+                {
+                    edges2.Add(edge.VerticesIds);
+                }
+                else
+                    Console.WriteLine("Edge of size " + edge.Vertices.Count + " cant be handled");
+            }
+
+            return edges3.Concat(edges2).Concat(edges1).ToList();
         }
 
 
