d2645cbc8135bd94c6259a19a4a97b470b56f20d
Luk-Fuchs <83712035+Luk-Fuchs@users.noreply.github.com>
2022-01-19 15:55:08 +0100

    First implementation of unweighted Blossom Algorithm

--- 3D Matching/Graph.cs
+++ 3D Matching/Graph.cs
@@ -16,6 +16,7 @@ namespace _3D_Matching
         }
         private List<Edge> _edges;
         public List<Vertex> Vertices;
+        public List<Vertex> ContractedVertices;
         public List<Edge> Edges { get => _edges!=null? _edges:GetAndSetEdges(); } 
         private Func<IList<Vertex>, bool> _isEdge;
         public bool IsEdge(IList<Vertex> potentialEdge)
@@ -219,6 +220,326 @@ namespace _3D_Matching
             var tmpGraph = new Graph(tmpEdges, inducedVertices.ToList());
             return tmpGraph;
         }
+        public void InitializeFor2DMatchin(List<Edge> initialMatching = null, List<Edge> contractingEdges = null)
+        {
+            SetVertexAdjEdges();
+            ContractedVertices = new List<Vertex>();
+            
+
+            foreach (var vertex in Vertices)
+            {
+                vertex.NeighboursFor2DMatching = new List<Vertex>();
+                foreach (var edge in vertex.AdjEdges)
+                {
+                    if (edge.Vertices.Count != 2)
+                        continue;
+                    vertex.NeighboursFor2DMatching.Add(edge.Vertices[0] == vertex ? edge.Vertices[1] : edge.Vertices[0]);
+                }
+            }
+
+            if (contractingEdges != null)
+            {
+                for (int i = 0; i < contractingEdges.Count; i++)
+                {
+                    var contractingEdge = contractingEdges[i];
+
+                    var contractedVertex = new Vertex(-i - 1);
+                    contractedVertex.OriginalVertex0 = contractingEdge.Vertices[0];
+                    contractedVertex.OriginalVertex1 = contractingEdge.Vertices[0];
+                    
+                    ContractedVertices.Add(contractedVertex);
+
+                    var vertex0 = contractingEdge.Vertices[0];
+                    var vertex1 = contractingEdge.Vertices[1];
+
+                    vertex0.IsContracted = true;
+                    vertex1.IsContracted = true;
+
+                    //vertex.VerticesFor3DEdge = new List<Vertex>();
+                    foreach (var edge in vertex0.AdjEdges) //speed: nicht alle müssen durchlaufen werden unfd forschleife
+                    {
+                        if (edge.Vertices.Count == 1 || edge == contractingEdge)
+                            continue;
+                        if (edge.Vertices.Count == 2)
+                        {
+                            edge.Vertices[0].NeighboursFor2DMatching.Remove(edge.Vertices[1]);
+                            edge.Vertices[1].NeighboursFor2DMatching.Remove(edge.Vertices[0]);
+                        }
+                        if (edge.Vertices.Count == 3)
+                        {
+                            if ((edge.Vertices[0] == vertex0 && edge.Vertices[1] == vertex1) || (edge.Vertices[1] == vertex0 && edge.Vertices[0] == vertex1))
+                            {
+                                edge.Vertices[2].NeighboursFor2DMatching.Add(contractedVertex);
+                                contractedVertex.NeighboursFor2DMatching.Add(edge.Vertices[2]);
+                            }
+                            else if ((edge.Vertices[2] == vertex0 && edge.Vertices[1] == vertex1) || (edge.Vertices[1] == vertex0 && edge.Vertices[2] == vertex1))
+                            {
+                                edge.Vertices[0].NeighboursFor2DMatching.Add(contractedVertex);
+                                contractedVertex.NeighboursFor2DMatching.Add(edge.Vertices[0]);
+                            }
+                            else if ((edge.Vertices[2] == vertex0 && edge.Vertices[0] == vertex1) || (edge.Vertices[0] == vertex0 && edge.Vertices[2] == vertex1))
+                            {
+                                edge.Vertices[1].NeighboursFor2DMatching.Add(contractedVertex);
+                                contractedVertex.NeighboursFor2DMatching.Add(edge.Vertices[1]);
+                            }
+                        }
+                    }
+                    foreach (var edge in vertex0.AdjEdges)
+                    {
+                        if (edge.Vertices.Count == 1 || edge == contractingEdge)
+                            continue;
+                        if (edge.Vertices.Count == 2)
+                        {
+                            edge.Vertices[0].NeighboursFor2DMatching.Remove(edge.Vertices[1]);
+                            edge.Vertices[1].NeighboursFor2DMatching.Remove(edge.Vertices[0]);
+                        }
+                    }
+                }
+            }
+
+            if (initialMatching != null)
+            {
+                for (int i = 0; i < initialMatching.Count; i++)
+                {
+                    var edge = initialMatching[i];
+                    if (edge.Vertices.Count == 2)
+                    {
+                        var vertex0 = edge.Vertices[0];
+                        var vertex1 = edge.Vertices[1];
+
+                        vertex0.MatchedVertex = vertex1;
+                        vertex1.MatchedVertex = vertex0;
+                    }
+                }
+
+            }
+        }
+        public (List<Edge> maxMmatching, List<Vertex> uncoveredVertices) GetMaximum2DMatching()
+        {
+            var potentialRoots = Vertices.Concat(ContractedVertices).Where(_=>_.MatchedVertex==null).ToList();
+            while (potentialRoots.Count > 0)
+            {
+                var newRoot = potentialRoots.First();
+                newRoot.IsInTree = true;
+                var plusTreeVertices = new List<Vertex>();
+                plusTreeVertices.Add(newRoot);
+                potentialRoots.RemoveAt(0);
+                var stack = new List<Vertex>();
+                stack.Add(newRoot);
+
+                var augmentationHasBeenPerformed = false;
+                int blossomCount = 0;
+
+                while (!augmentationHasBeenPerformed && stack.Count > 0)
+                {
+                    var activeVertex = stack.First();
+                    stack.Remove(activeVertex);
+
+                    for (int neighbourIndex = 0; neighbourIndex < activeVertex.NeighboursFor2DMatching.Count; neighbourIndex++)
+                    {
+                        var activeNeighbour = activeVertex.NeighboursFor2DMatching[neighbourIndex];
+                        if (activeNeighbour.IsInTree && (activeNeighbour.BlossomIndex != activeVertex.BlossomIndex || activeNeighbour.BlossomIndex + activeVertex.BlossomIndex==0) && (activeNeighbour.Predecessor == null || activeNeighbour.OddPath != null))
+                        { // do blossom building.                         => no blossominternal edge                                                                                            =>is of type plus               => is blossom of tpye plus          
+                            
+                            CalculateAndSetOddPaths(activeVertex, activeNeighbour, stack,plusTreeVertices, ref blossomCount);
+
+                        }
+                        else if (activeNeighbour.IsInTree == false)
+                        {
+                            if (activeNeighbour.MatchedVertex == null)
+                            {// Augment
+
+                                activeNeighbour.Predecessor = activeVertex;
+                                potentialRoots.Remove(activeNeighbour);
+                                (Vertex, List<Vertex>) upwardsInfo = (activeNeighbour, null);
+                                while (upwardsInfo != (null, null))
+                                {
+                                    upwardsInfo = DoNextAugmentationStep(upwardsInfo);
+                                }
+
+                                ResetTree(plusTreeVertices);
+
+                                augmentationHasBeenPerformed = true;
+                                break;
+                            }
+                            else
+                            {// grow
+                                var newPlusVertex = activeNeighbour.MatchedVertex;
+                                stack.Add(newPlusVertex);
+                                activeNeighbour.Predecessor = activeVertex;
+                                plusTreeVertices.Add(newPlusVertex);
+
+                                newPlusVertex.IsInTree = true;
+                                activeNeighbour.IsInTree = true;
+                            }
+                        }
+                    }
+
+                }
+            }
+            var resMatching = new List<Edge>();
+            var uncoveredVertices = new List<Vertex>();
+            foreach(var vertex in Vertices.Concat(ContractedVertices))
+            {
+                if (vertex.MatchedVertex != null)
+                {
+                    if (vertex.Id < vertex.MatchedVertex.Id)
+                    {
+                        if(vertex.Id<0)//contracted Vertex
+                        {
+                            resMatching.Add(new Edge(new List<Vertex> { vertex.OriginalVertex0, vertex.OriginalVertex1, vertex.MatchedVertex }));
+                        }
+                        else //normal vertex
+                        {
+                            resMatching.Add(new Edge(new List<Vertex> { vertex, vertex.MatchedVertex }));
+                        }
+
+                    }
+                }
+                else
+                {
+                    if (vertex.Id < 0)
+                    {
+                        resMatching.Add(new Edge(new List<Vertex> { vertex.OriginalVertex0, vertex.OriginalVertex1}));
+                    }
+                    else
+                    {
+                        uncoveredVertices.Add(vertex);
+                    }
+                }
+            }
+            return (resMatching, uncoveredVertices);
+        }
+
+        private static void CalculateAndSetOddPaths(Vertex activeVertex, Vertex activeNeighbour, List<Vertex> stack, List<Vertex> plusTreeVertices, ref int blossomCount)
+        {
+            //search commom vertex
+            var hashSet = new HashSet<Vertex>();
+            Vertex x = activeVertex;
+            while (x != null)
+            {
+                hashSet.Add(x);
+                x = x.MatchedVertex;
+                if (x == null)
+                    break;
+                x = x.Predecessor;
+            }
+            x = activeNeighbour;
+            while (!hashSet.Contains(x))
+            {
+                x = x.MatchedVertex;
+                x = x.Predecessor;
+            }
+            var commonVertex = x;
+
+
+            //compute BlossomPath
+            var leftPathUpwards = new List<Vertex>();
+            x = activeVertex;
+            while (x != commonVertex)
+            {
+                leftPathUpwards.Add(x);
+                x = x.MatchedVertex;
+                leftPathUpwards.Add(x);
+                x = x.Predecessor;
+            }
+            leftPathUpwards.Add(x);
+
+            var rightPathUpwards = new List<Vertex>();
+            x = activeNeighbour;
+            while (x != commonVertex)
+            {
+                rightPathUpwards.Add(x);
+                x = x.MatchedVertex;
+                rightPathUpwards.Add(x);
+                x = x.Predecessor;
+            }
+            rightPathUpwards.Add(x);
+            var leftPathDownwards = leftPathUpwards.ToList();
+            leftPathDownwards.Reverse();
+            var rightPathDownwards = rightPathUpwards.ToList();
+            rightPathDownwards.Reverse();
+
+
+            //set oddPaths
+            for(int i = 1; i< leftPathDownwards.Count; i += 2)
+            {
+                x = leftPathDownwards[i];
+                x.OddPath = leftPathDownwards.Skip(i + 1).Concat(rightPathUpwards).ToList();
+                stack.Add(x);
+            }
+            for (int i = 1; i < rightPathDownwards.Count; i += 2)
+            {
+                x = rightPathDownwards[i];
+                x.OddPath = rightPathDownwards.Skip(i + 1).Concat(leftPathUpwards).ToList();
+                stack.Add(x);
+            }
+
+            var blossomIndizesToReplace = leftPathDownwards.Concat(rightPathDownwards).Select(_ => _.BlossomIndex).Distinct().ToHashSet();
+            blossomCount++;
+
+            foreach(var plusVertex in plusTreeVertices)
+            {
+                if (blossomIndizesToReplace.Contains(plusVertex.BlossomIndex))
+                    plusVertex.BlossomIndex = blossomCount;
+            }
+            foreach (var plusVertex in leftPathDownwards)
+            {
+                plusVertex.BlossomIndex = blossomCount;
+            }
+            foreach (var plusVertex in rightPathDownwards)
+            {
+                plusVertex.BlossomIndex = blossomCount;
+            }
+        }
+
+        private static void ResetTree(List<Vertex> plusTreeVertices)
+        {
+            foreach (var plusVertex in plusTreeVertices)
+            {
+                plusVertex.IsInTree = false;
+                plusVertex.OddPath = null;
+                plusVertex.Predecessor = null;
+                plusVertex.BlossomIndex = 0;
+                if (plusVertex.MatchedVertex == null)
+                    continue;
+                plusVertex.MatchedVertex.IsInTree = false;
+                plusVertex.MatchedVertex.OddPath = null;
+                plusVertex.MatchedVertex.Predecessor = null;
+                plusVertex.MatchedVertex.BlossomIndex = 0;
+            }
+        }
+
+        private (Vertex, List<Vertex>) DoNextAugmentationStep((Vertex matchedVertex, List<Vertex> oddPath) upwardsInfo)
+        {
+            (Vertex matchedVertex, List<Vertex> oddPath) newUpwardsInfo = (null, null);
+            if (upwardsInfo.oddPath != null)
+            {
+                newUpwardsInfo.matchedVertex = upwardsInfo.oddPath.Last().MatchedVertex;
+                newUpwardsInfo.oddPath = upwardsInfo.oddPath.Last().OddPath;
+
+                for (int i = 0; i < upwardsInfo.oddPath.Count; i += 2)
+                {
+                    var vertex0 = upwardsInfo.oddPath[i];
+                    var vertex1 = upwardsInfo.oddPath[i + 1];
+
+                    vertex0.MatchedVertex = vertex1;
+                    vertex1.MatchedVertex = vertex0;
+                }
+            }
+            else
+            {
+                var lowerVertex = upwardsInfo.matchedVertex;
+                var higherVertex = upwardsInfo.matchedVertex.Predecessor;
+
+                newUpwardsInfo.matchedVertex = higherVertex.MatchedVertex;
+                newUpwardsInfo.oddPath = higherVertex.OddPath;
+
+                lowerVertex.MatchedVertex = higherVertex;
+                higherVertex.MatchedVertex = lowerVertex;
+            }
+            return newUpwardsInfo;
+        }
     }
 
     public class Edge
@@ -298,6 +619,16 @@ namespace _3D_Matching
     }
     public class Vertex 
     {
+        public List<Vertex> NeighboursFor2DMatching;
+        public Vertex Predecessor;
+        public bool IsInTree;
+        public List<Vertex> OddPath;
+        public Vertex MatchedVertex;
+        public bool IsContracted;
+        public int BlossomIndex;
+
+
+
         public bool IsCovered = false;
         public int TimesCovered = 0;
         public int[] Interval;
@@ -311,6 +642,8 @@ namespace _3D_Matching
             this.Id = id;
         }
         public int Id;
+        internal Vertex OriginalVertex0;
+        internal Vertex OriginalVertex1;
 
         public bool Equals(Vertex otherObject)
         {
