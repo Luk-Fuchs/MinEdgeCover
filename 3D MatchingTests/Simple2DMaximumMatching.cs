007213a03d9e7285a3209005fb9ee0c793159925
Luk-Fuchs <83712035+Luk-Fuchs@users.noreply.github.com>
2022-01-22 20:24:25 +0100

    Adjust te expected Result

--- 3D MatchingTests/Simple2DMaximumMatching.cs
+++ 3D MatchingTests/Simple2DMaximumMatching.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System;
 using _3D_Matching;
+using System.Collections.Generic;
 
 namespace _3D_MatchingTests
 {
@@ -16,7 +17,7 @@ namespace _3D_MatchingTests
             graph.InitializeFor2DMatchin();
             var matchingInfo = graph.GetMaximum2DMatching();
 
-            Assert.AreEqual(2, matchingInfo.maxMmatching.Count);
+            Assert.AreEqual(3, matchingInfo.maxMmatching.Count);
         }
         [TestMethod]
         public void TreeGetsResettetCorrectly()
@@ -26,7 +27,55 @@ namespace _3D_MatchingTests
             graph.InitializeFor2DMatchin();
             var matchingInfo = graph.GetMaximum2DMatching();
 
+            Assert.AreEqual(3, matchingInfo.maxMmatching.Count);
+        }
+        [TestMethod]
+        public void UnmatchedLeave()
+        {
+            var graphString = "3;0->1;1->2;0";
+            var graph = Graph.BuildGraphString(graphString);
+            graph.InitializeFor2DMatchin();
+            var matchingInfo = graph.GetMaximum2DMatching();
+
             Assert.AreEqual(2, matchingInfo.maxMmatching.Count);
+            Assert.AreEqual(1, matchingInfo.uncoveredVertices.Count);
+            Assert.AreEqual(graph.Vertices[0], matchingInfo.uncoveredVertices[0]);
+        }
+        [TestMethod]
+        public void UnmatchedRoot()
+        {
+            var graphString = "3;0->1;1->2;2";
+            var graph = Graph.BuildGraphString(graphString);
+            graph.InitializeFor2DMatchin();
+            var matchingInfo = graph.GetMaximum2DMatching();
+
+            Assert.AreEqual(2, matchingInfo.maxMmatching.Count);
+            Assert.AreEqual(1, matchingInfo.uncoveredVertices.Count);
+            Assert.AreEqual(graph.Vertices[2], matchingInfo.uncoveredVertices[0]);
+        }
+        [TestMethod]
+        public void UnmatchedInteriorVertexInFlatTree()
+        {
+            var graphString = "5;0->1;1->2;2->3;3->4;2";
+            var graph = Graph.BuildGraphString(graphString);
+            graph.InitializeFor2DMatchin();
+            var matchingInfo = graph.GetMaximum2DMatching();
+
+            Assert.AreEqual(3, matchingInfo.maxMmatching.Count);
+            Assert.AreEqual(1, matchingInfo.uncoveredVertices.Count);
+            Assert.AreEqual(graph.Vertices[2], matchingInfo.uncoveredVertices[0]);
+        }
+        [TestMethod]
+        public void UnmatchedMinusVertexInBlossom()
+        {
+            var graphString = "3;0->1;0->2;1->2;0";
+            var graph = Graph.BuildGraphString(graphString);
+            graph.InitializeFor2DMatchin();
+            var matchingInfo = graph.GetMaximum2DMatching();
+
+            Assert.AreEqual(2, matchingInfo.maxMmatching.Count);
+            Assert.AreEqual(1, matchingInfo.uncoveredVertices.Count);
+            Assert.AreEqual(graph.Vertices[0], matchingInfo.uncoveredVertices[0]);
         }
     }
 }
--- 3D MatchingTests/Simple2DPerfektMatching.cs
+++ 3D MatchingTests/Simple2DPerfektMatching.cs
@@ -37,7 +37,50 @@ namespace _3D_MatchingTests
             graph.InitializeFor2DMatchin();
             var matchingInfo = graph.GetMaximum2DMatching();
 
+            Assert.AreEqual(4, matchingInfo.maxMmatching.Count);
+        }
+        [TestMethod]
+        public void ProblemCase()
+        {
+            var graphString = "12;0->3;0->4;0->7;0->8;1->2;1->5;2->3;2->4;2->5;2->9;3->4;3->10;4->7;6->8;9->10;9->11;0;1;2;3;4;5;6;7;8;9;10;11";
+            var graph = Graph.BuildGraphString(graphString);
+            graph.InitializeFor2DMatchin();
+            var matchingInfo = graph.GetMaximum2DMatching();
+
+            Assert.AreEqual(6, matchingInfo.maxMmatching.Count);
+        }
+        
+        [TestMethod]
+        public void ProblemCase2()
+        {
+            var graphString = "5;0->1;0->2;0->3;0->4;1->4;2->3;0;1;2;3;4";
+            var graph = Graph.BuildGraphString(graphString);
+            graph.InitializeFor2DMatchin();
+            var matchingInfo = graph.GetMaximum2DMatching();
+
             Assert.AreEqual(3, matchingInfo.maxMmatching.Count);
+        }
+        
+        [TestMethod]
+        public void ProblemCase3()
+        {
+            var graphString = "6;0->3;0->4;0->5;1->2;1->3;1->4;1->5;2->3;2->4;0;1;2;3;4;5";
+            var graph = Graph.BuildGraphString(graphString);
+            graph.InitializeFor2DMatchin();
+            var matchingInfo = graph.GetMaximum2DMatching();
+
+            Assert.AreEqual(3, matchingInfo.maxMmatching.Count);
+        }
+        
+        [TestMethod]
+        public void ProblemCase4()
+        {
+            var graphString = "16;0->4;0->8;1->2;4->6;5->6;5->7;5->10;6->13;7->11;7->15;8->10;9->11;10->14;11->13;13->15;0;1;2;3;4;5;6;7;8;9;10;11;12;13;14;15";
+            var graph = Graph.BuildGraphString(graphString);
+            graph.InitializeFor2DMatchin();
+            var matchingInfo = graph.GetMaximum2DMatching();
+
+            Assert.AreEqual(9, matchingInfo.maxMmatching.Count);
         }
     }
 }
