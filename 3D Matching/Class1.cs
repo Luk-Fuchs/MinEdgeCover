48f49785920d4144a0f3221c831a9d6c1b55677b
Luk-Fuchs <83712035+Luk-Fuchs@users.noreply.github.com>
2022-02-12 14:31:39 +0100

    Added Extension for fast shuffle

---
+++ 3D Matching/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3D_Matching
+{
+    public static class Extensions
+    {
+        public static void Shuffle<T>(this List<T> list)
+        {
+            int n = list.Count;
+            var random = new Random();
+            for (int i = 0; i < list.Count; i++)
+            {
+                int k = random.Next(i, n);
+                T value = list[k];
+                list[k] = list[i];
+                list[i] = value;
+            }
+        }
+    }
+}
