49e42306f5c1280cf5ddc4ae43b5c82a98d2e503
Luk-Fuchs <83712035+Luk-Fuchs@users.noreply.github.com>
2022-02-28 18:14:41 +0100

    xlabel is now in houres

--- PythonPlots/plot_intervals.py
+++ PythonPlots/plot_intervals.py
@@ -10,7 +10,30 @@ f = open("C:/Users/LFU/Desktop/tmp/intervals.csv", "r")
 
 intervals = eval(f.read())
 plt.figure()
+plt.xlabel("Uhrzeit")
+plt.ylabel("Dienste")
 for row_index, row in enumerate(intervals):
+    #for x in row:
+    #    plt.plot(x,[row_index,row_index])
+    #if(len(row)==1):
+    #    plt.plot([10600,10700],[row_index,row_index], "r")
+    #if(len(row)==2):
+    #    plt.plot([10600,10700],[row_index,row_index], "y")
+    #if(len(row)==3):
+    #    plt.plot([10600,10700],[row_index,row_index], "g")
     for x in row:
-        plt.plot(x,[row_index,row_index])
+        if(len(row)==1):
+            plt.plot(x,[row_index,row_index], "r")
+        if(len(row)==2):
+            plt.plot(x,[row_index,row_index],"y")
+        if(len(row)==3):
+            plt.plot(x,[row_index,row_index], "g")
+
+
+plt.plot([10000,10000],[1,1],"r" ,label = "ein-teilig")
+plt.plot([10000,10000],[1,1],"y" ,label = "zwei-teilig")
+plt.plot([10000,10000],[1,1],"g" ,label = "drei-teilig")
+time = [0,6,12,18,24]
+plt.xticks([x*60*60 for x in time],time)
+plt.legend()
 plt.show()
