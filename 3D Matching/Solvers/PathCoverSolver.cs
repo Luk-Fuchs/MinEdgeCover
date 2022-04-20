f2579393746c9d09ab44274f479e3629d0b83a22
Luk-Fuchs <83712035+Luk-Fuchs@users.noreply.github.com>
2022-04-20 09:42:17 +0200

    Added Solver to calculate minimal Pathcover

---
+++ 3D Matching/Solvers/PathCoverSolver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3D_Matching.Solvers
+{
+    public class PathCoverSolver
+    {
+
+        public static List<List<Vertex>> CalculatePathCoverOfNonOverlappingGraph(List<Vertex> vertices)
+        {
+            var leftNewVertices = vertices.Select(_ => new Vertex(_.Id)).ToList();
+            var rightNewVertices = vertices.Select(_ => new Vertex(10_000+_.Id)).ToList();
+            var bipartEdges = new List<Edge>();
+
+            for(int l = 0; l<leftNewVertices.Count;l++)
+            {
+            for(int r = 0; r<rightNewVertices.Count;r++)
+                {
+                    if (!vertices[l].Intersects(vertices[r]) && vertices[l].Interval[0]< vertices[r].Interval[0])
+                    {
+                        bipartEdges.Add(new Edge(new List<Vertex>() { leftNewVertices[l], rightNewVertices[r] }));
+                    }
+                }
+            }
+            var helpGraph = new Graph(bipartEdges, leftNewVertices.Concat(rightNewVertices).ToList());
+            helpGraph.ResetVertexAdjEdges();
+            helpGraph.InitializeFor2DMatchin();
+            var helpMatching= helpGraph.GetMaximum2DMatching();
+
+            var pathcover = new List<List<Vertex>>();
+
+            var unconsideredVertices = vertices.ToList();
+            //works only if ordered
+            while (unconsideredVertices.Count != 0)
+            {
+                var activeVertex = unconsideredVertices[0];
+                unconsideredVertices.Remove(activeVertex);
+                pathcover.Add(new List<Vertex>() { activeVertex });
+                while (leftNewVertices[activeVertex.Id].MatchedVertex != null)
+                {
+                    activeVertex = vertices[leftNewVertices[activeVertex.Id].MatchedVertex.Id % 10_000];
+                    unconsideredVertices.Remove(activeVertex);
+                    pathcover.Last().Add(activeVertex);
+                }
+            }
+
+            var allVerticesInPathcover = pathcover.SelectMany(_ => _).ToList();
+            pathcover.AddRange(vertices.Where(_ => !allVerticesInPathcover.Contains(_)).Select(_ => new List<Vertex>() { _ }));
+            return pathcover;
+        }
+    }
+}
