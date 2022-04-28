72b17f8e3207ae56846e1d8d0369dd9fd597e5e1
Luk-Fuchs <83712035+Luk-Fuchs@users.noreply.github.com>
2022-04-28 10:23:07 +0200

    some tests for pathcover

--- ConsoleApp1/Analyse.cs
+++ ConsoleApp1/Analyse.cs
@@ -21,12 +21,170 @@ namespace test
                                                         forceCompletness: false,
                                                         removeDegreeOne: true,
                                                         addAllPossibleEdges: false,
-                                                        iterations: 2);
-            var graph = graphs[0];
+                                                        iterations: 400);
+
+            var maxDegree = graphs.Select(g => g.Vertices.Select(_ => _.AdjEdges.Count).Max()).Max();
+            var vertexOfDegree = new int[maxDegree + 1];
+            var haveToVertexOfDegree = new int[maxDegree + 1];
+
+            foreach (var graph in graphs)
+            {
+                var haveToEdges = ComputeuniquePairingEdges(graph);
+                var haveToDegrees = haveToEdges.SelectMany(_ => _.Vertices).Select(_ => _.AdjEdges.Count).OrderBy(_ => _).ToList();
+                var allDegrees = graph.Vertices.Select(_ => _.AdjEdges.Count).OrderBy(_ => _).ToList();
+                foreach (var d in haveToDegrees)
+                    haveToVertexOfDegree[d]++;
+                foreach (var d in allDegrees)
+                    vertexOfDegree[d]++;
+                Console.WriteLine(haveToEdges.Count);
+            }
+            var distributon = new List<double>();
+            for (int i = 0; i < maxDegree + 1; i++)
+            {
+                if (vertexOfDegree[i] == 0)
+                    distributon.Add(0);
+                else
+                    distributon.Add((haveToVertexOfDegree[i] + 0.000001) / vertexOfDegree[i]);
+            }
+            //Plot.CreateFigure(distributon);
+            Plot.CreateFigure(distributon,title:"Anteil eindeutiger Knoten nach Grad (Datensatz 1)", xLable:"Knotengrad",yLable:"relative Anzahl fester Knoten");
+
+            //var graph = graphs[0];
+            //graph.InitializeFor2DMatchin();
+            //var pathcover = PathCoverSolver.CalculatePathCoverOfNonOverlappingGraph(graph.Vertices);
+            //Plot.CreateIntervals(pathcover.Select(_ => new Edge(_)).ToList());
+
+            //var haveToEdges = ComputeuniquePairingEdges(graph);
+            //var haveToVertices = haveToEdges.SelectMany(_ => _.Vertices).ToList();
+
+            //var valideEdges = pathcover.Where(_ => _.Count < 4).Select(_ => new Edge(_)).Where(_ => graph.Edges.Contains(_)).ToList();
+            //Plot.CreateIntervals(valideEdges);
+            ////foreach (var path in pathcover)
+            ////{
+            ////    foreach (var v in path.ToList())
+            ////    {
+            ////        if (!haveToVertices.Contains(v))
+            ////        {
+            ////            path.Remove(v);
+            ////        }
+            ////    }
+            ////}
+            ////Console.WriteLine(pathcover.Where(_ => _.Count < 4).Select(_ => new Edge(_)).Where(_ => graph.Edges.Contains(_)).Count());
+            ////Plot.CreateIntervals(haveToEdges);
+
+            //var parameters = new Dictionary<String, double>();
+            //Console.WriteLine("--------------------------------------");
+            //var solver = new MIP();
+            //solver.initialize(graph);
+            //var res1 = solver.Run(parameters);
+            //Console.WriteLine("res1: " + (res1.cover.Count));
 
 
-            //var canBeEdges = ComputePossiblePairingEdges(graph);
-            var haveToEdges =ComputeuniquePairingEdges(graph);
+            //////valideEdges = valideEdges.Where(_ => _.VertexCount > 2).ToList();
+
+            //////var verticesToDelete = valideEdges.SelectMany(_ => _.Vertices).ToList();
+            //////var graph2 = graph.GenerateInducedSubgraph(graph.Vertices.Except(verticesToDelete).ToList());
+
+            ////solver.initialize(graph2);
+            ////var res2 = solver.Run(parameters);
+            ////Console.WriteLine("res2: " + (res2.cover.Count + valideEdges.Count));
+
+
+            ////var sizeDistribuion = new int[8];
+            ////for(int i = 0; i < sizeDistribuion.Length; i++)
+            ////{
+            ////    sizeDistribuion[i] = pathcover.
+            ////}
+            //var test = pathcover.GroupBy(_ => _.Count).Select(_ => (_.Key, _.Count())).OrderBy(_=>_.Key).ToList();
+            //;
+
+            //var subEdges = new List<Edge>();
+            //foreach(var path in pathcover)
+            //{
+            //    var edge = DetectValideSubEdge(path, graph);
+            //    if ( edge!= null)
+            //    {
+            //        subEdges.Add(edge);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(path.Count);
+            //    }
+
+            //}
+
+
+
+            //var verticesToDelete = subEdges.SelectMany(_ => _.Vertices).ToList();
+            //var graph2 = graph.GenerateInducedSubgraph(graph.Vertices.Except(verticesToDelete).ToList());
+            //solver.initialize(graph2);
+            //var res2 = solver.Run(parameters);
+            //Console.WriteLine("res2: " + (res2.cover.Count + subEdges.Count));
+
+
+
+            //foreach (var v in subEdges.SelectMany(_ => _.Vertices))
+            //{
+            //    foreach(var n in v.NeighboursFor2DMatching)
+            //    {
+            //        n.NeighboursFor2DMatching.Remove(v);
+            //    }
+            //    v.NeighboursFor2DMatching.Clear();
+            //}
+
+            
+            //pathcover = PathCoverSolver.CalculatePathCoverOfNonOverlappingGraph(graph.Vertices);
+
+
+            //var subEdges2 = new List<Edge>();
+            //foreach (var path in pathcover)
+            //{
+            //    var edge = DetectValideSubEdge(path, graph);
+            //    if (edge != null)
+            //    {
+            //        subEdges2.Add(edge);
+            //    }
+            //    else
+            //    {
+            //        //Console.WriteLine(path.Count);
+            //    }
+
+            //}
+
+            //var subedes3 = subEdges.Concat(subEdges2).ToList();
+            //var verticesToDelete2 = subedes3.SelectMany(_ => _.Vertices).ToList();
+            //var graph3 = graph.GenerateInducedSubgraph(graph.Vertices.Except(verticesToDelete2).ToList());
+            //solver.initialize(graph2);
+            //var res3 = solver.Run(parameters);
+            //Console.WriteLine("res3: " + (res3.cover.Count + subedes3.Count));
+
+
+
+            //;
+            ////graph2.InitializeFor2DMatchin();
+            ////var pathcover2 = PathCoverSolver.CalculatePathCoverOfNonOverlappingGraph(graph2.Vertices);
+            ////Plot.CreateIntervals(pathcover2.Select(_ => new Edge(_)).ToList());
+            ////var valideEdges2 = pathcover2.Where(_ => _.Count < 4).Select(_ => new Edge(_)).Where(_ => graph.Edges.Contains(_)).ToList();
+
+
+
+
+            //Plot.CreateIntervals(pathcover.Select(_ =>  new Edge(_)).ToList());
+        }
+
+        private static Edge DetectValideSubEdge (List<Vertex> vertices, Graph graph)
+        {
+            if (vertices.Count <= 3)
+                return null;
+            for(int i = 0; i< vertices.Count - 3; i++)
+            {
+                var tmpEdge = new Edge(vertices.Skip(i).Take(3).ToList());
+                if (graph.Edges.Contains(tmpEdge))
+                {
+                    return tmpEdge;
+                }
+            }
+            return null;
         }
 
         private static List<Edge> ComputeuniquePairingEdges(Graph graph)
